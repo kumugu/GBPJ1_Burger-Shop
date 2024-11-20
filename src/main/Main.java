0c2871863279797ab982b74a6ca6345b90e99a7d
jungeun-am <jungeun0054@gmail.com>
2024-11-20 17:30:11 +0900

    삭제

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,6 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("안녕하세요 햄버거 가게입니다.");
-        System.out.println("맛있게 드세용");
+        
     }
 }
